03c616178973a6b6f7dc76b6efd100e6773c5524
JoelAlgera <Joel.Algera@student.ufv.ca>
2025-11-06 09:32:33 -0800

    Car Selection
    Added a car selection menu.  Now when you select a car and a track it calls the track scene and exports the selected car scene as a global variable. This means each track will need a spawn point and check that global var and spawn it. Works pretty good tho imo.

--- assginment-2/alpine.gd
+++ assginment-2/alpine.gd
@@ -1,5 +1,14 @@
 extends Node3D
 
+@onready var car_spawn = $SpawnPoint
+
 func _input(event):
 	if event.is_action_pressed("to_menu"):
 		get_tree().change_scene_to_file("res://startMenu.tscn")
+
+func _ready():
+	var chosen_car = load(GlobalData.selected_car_scene)
+	var car = chosen_car.instantiate()
+	add_child(car)
+	car.global_transform = car_spawn.global_transform
+	print("spawned", GlobalData.selected_car_scene)
--- assginment-2/start_menu.gd
+++ assginment-2/start_menu.gd
@@ -6,33 +6,58 @@ var tracks = [
 	{"name": "Alpine Track", "scene": "res://alpine.tscn", "preview": "res://textures/alpinePreview.png"},
 	{"name": "City Freeroam", "scene": "res://cityRace.tscn", "preview": "res://textures/cityPreview.png"}	
 ]
+
+var cars = [
+	{"name": "toyota", "scene": "res://Toyota/Toyota.tscn", "preview": "res://Toyota/Root Scene_Palette.jpg"},
+	{"name": "sporty", "scene": "res://SportsCar/SportCar.tscn", "preview": "res://SportsCar/Sport_Lamborginhi Aventador_diffuse.jpg"}
+]
 var current_track = 0
+var current_car = 0
 
 @onready var track_label = $TrackLabel
 @onready var play_button = $playButton
-@onready var track_preview = $HBoxContainer/Preview
+@onready var track_preview = $TrackSelector/Preview
+@onready var car_label = $CarLabel
+@onready var car_preview = $CarSelector/CarPreview
 
 func _ready():
-	var track_buttons = [$HBoxContainer/leftTrack, $HBoxContainer/rightTrack]
-	track_buttons[0].pressed.connect(_on_prev_track)
-	track_buttons[1].pressed.connect(_on_next_track)
-	_update_labels()
+	var track_buttons = [$TrackSelector/leftTrack, $TrackSelector/rightTrack]
+	track_buttons[0].pressed.connect(_prev_track)
+	track_buttons[1].pressed.connect(_next_track)
+	update_labels()
+	
+	var car_buttons = [$CarSelector/leftCar, $CarSelector/rightCar]
+	car_buttons[0].pressed.connect(_prev_car)
+	car_buttons[1].pressed.connect(_next_car)
 	
 	play_button.pressed.connect(_on_play_pressed)
 	
-func _update_labels():
+func update_labels():
 	track_label.text = tracks[current_track].name
 	track_preview.texture = load(tracks[current_track].preview)
+	car_label.text = cars[current_car].name
+	car_preview.texture = load(cars[current_car].preview)
 	
 	
-func _on_prev_track():
+	
+func _prev_track():
 	current_track = (current_track - 1 + tracks.size()) % tracks.size()
-	_update_labels()
+	update_labels()
 
-func _on_next_track():
+func _next_track():
 	current_track = (current_track + 1 + tracks.size()) % tracks.size()
-	_update_labels()
+	update_labels()
+	
+func _prev_car():
+	current_car = (current_car - 1 + cars.size()) % cars.size()
+	update_labels()
+
+func _next_car():
+	current_car = (current_car + 1 + cars.size()) % cars.size()
+	print("moving right!")
+	update_labels()
 	
 func _on_play_pressed():
+	GlobalData.selected_car_scene = cars[current_car].scene
 	get_tree().change_scene_to_file(tracks[current_track].scene)
-	print("calling", tracks[current_track].scene )
+	print("calling", tracks[current_track].scene, "with ", cars[current_car].name)
