a59da1b97befbd2635e8f88789b8751f5ba9aad7
Ryan <ryankaustin2@gmail.com>
2025-11-10 14:44:56 -0800

    Revert "Dune Additions"
    This reverts commit 9f5c96448487510a99b787f84c9a26bb969e0b1e.

--- assginment-2/path_follow_3d.gd
+++ assginment-2/path_follow_3d.gd
@@ -1,19 +1,10 @@
 extends PathFollow3D
 
-@export var max_speed := 0.01      # target max speed
-@export var acceleration := 0.009  # how quickly it accelerates per second
+@export var speed := 0.005  # normalized units per second
 
-var current_speed := 0.0
-
-func _process(delta: float) -> void:
-	# Accelerate like a real car until reaching top speed
-	if current_speed < max_speed:
-		current_speed += acceleration * delta
-		current_speed = min(current_speed, max_speed)  # cap at max speed
-
-	# Move along the path
-	progress_ratio += current_speed * delta / 2.3
-
-	# Loop around
+func _process(delta):
+	# advance along the path
+	progress_ratio += speed * delta/3
+	# loop around
 	if progress_ratio > 1.0:
 		progress_ratio = 0.0
