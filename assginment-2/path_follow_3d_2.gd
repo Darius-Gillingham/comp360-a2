677c70dcbc39363dddc9eca993460f446bc7ad9f
Ryan <ryankaustin2@gmail.com>
2025-11-10 18:04:13 -0800

    More Changes

--- assginment-2/path_follow_3d_2.gd
+++ assginment-2/path_follow_3d_2.gd
@@ -1,8 +1,8 @@
 extends PathFollow3D
 
 @export var speed := 0.005  # normalized units per second
-@export var max_speed := 0.01      # target max speed
-@export var acceleration := 0.009  # how quickly it accelerates per second
+@export var max_speed := 0.0099      # target max speed
+@export var acceleration := 0.006  # how quickly it accelerates per second
 var current_speed := 0.0
 func _process(delta: float) -> void:
 	# Accelerate like a real car until reaching top speed
@@ -11,8 +11,8 @@ func _process(delta: float) -> void:
 		current_speed = min(current_speed, max_speed)  # cap at max speed
 
 	# Move along the path
-	progress_ratio += current_speed * delta / 2.3
+	progress_ratio += current_speed * delta / 2.2
 
-	# Loop aroSund
+	# Loop around
 	if progress_ratio > 1.0:
 		progress_ratio = 0.0
